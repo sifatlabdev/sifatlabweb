9bc44c2dfa0f3d9bcf47b549d943464bd0de898d
Jakh T <jakhmac@macbookair.mynetworksettings.com>
2025-12-07 10:57:43 -0500

    scroll to top added

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Header } from "./components/Header";
 import { About } from "./components/About";
 import { Lab } from "./components/Lab";
 import { Publications } from "./components/Publications";
-import {Research} from "./components/Research";
+import { Research } from "./components/Research";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <Router>
-    <div className="min-h-screen bg-background min-h-screen flex flex-col">
-      <Header/>
-      <main className='flex-1'>
-      <Routes>
-        <Route path="/" element={<Lab />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/publications" element={<Publications />} />
-        <Route path="/research" element={<Research />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      </main>
-      <Footer />
-    </div>
+      <div className='min-h-screen bg-background min-h-screen flex flex-col'>
+        <ScrollToTop />
+        <Header />
+        <main className='flex-1'>
+          <Routes>
+            <Route path='/' element={<Lab />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/publications' element={<Publications />} />
+            <Route path='/research' element={<Research />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
